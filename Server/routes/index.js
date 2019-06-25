bb7095a996cb8a306f77d862d80ed184424a9bc6
dokdokorea <7dpdud@naver.com>
2019-06-26 00:09:00 +0900

    connect server to mysql

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,32 +1,43 @@
-module.exports = function(app,Book)
-{
-  //GET ALL BOOKS
-  app.get('/api/books',function(req,res){
-    res.end();
-  });
-//GET SINGLE BOOK
-app.get('/api/books/:book_id',function(req,res){
-  res.end();
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+//mysql불러오기
+var mysql = require('mysql');
+
+//mysql커넥션 생성
+var connection = mysql.createConnection({
+  host: "172.30.1.37",
+  port: 3306,
+  user: "root",//계정 아이디
+  password: "1234",//계정 비밀번호
+  database: "yoitgeo" //접속할 db
 });
 
-//GET BOOK BY AUTHOR
-app.get('/api/books/author/:author',function(req,res){
-  res.end();
+//mysql접속
+connection.connect();
+
+//user라우터
+app.post('../index.html',function(req,res){
+
+  var attr_id = req.body.attr_id;
+  var geo_id = req.body.geo_id;
+  var name = req.body.name;
+  var comm = req.body.comm;
+
+  //SQL문 실행
+  connection.query("INSERT INTO geotable (attr_id,geo_id,name,comm) VALUES ('"+attr_id+"','"+geo_id+"','"+name+"',
+'"+comm+"')"
+function(error,result,fields){
+
+  if(error){ //에러 발생 시
+    res.send('err : '+ error)
+  }else { //실행 성공
+    console.log(attr_id+','+geo_id+','+name+','+comm);
+    res.send('success create geotable elements: '+attr_id+','+geo_id+','+name+','+comm);
+  }
 });
 
-//CREATE BOOK
-app.post('/api/books',function(req,res){
-  res.end();
-});
-
-//UPDATE THE BOOK
-app.put('/api/books/:book_id',function(req,res){
-  res.end();
-});
-
-//DELETE BOOK
-app.delete('/api/books/:book_id',function(req,res){
-  res.end();
-});
-
-}
+})
