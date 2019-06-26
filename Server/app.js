5f2fe930b887ff04ecfb4a45b3054a3551184e5f
dokdokorea <7dpdud@naver.com>
2019-06-26 23:01:18 +0900

    build success

--- Server/app.js
+++ Server/app.js
@@ -1,23 +1,15 @@
- var express = require('express');
- var http = require('http');
- var app = express();
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
-
- app.get('/',function(req,res){
-   res.send('Hello /');
- })
- app.get('/world.html',function(req,res){
-   res.send('Hello World!');
- });
-
-
+app.use(bodyParser.urlencoded({extended:true}));
 
 //mysql불러오기
 var mysql = require('mysql');
 
 //mysql커넥션 생성
 var connection = mysql.createConnection({
-  host: 'localhost',
+  host: "localhost",
   port: 3306,
   user: "root",//계정 아이디
   password: "1234",//계정 비밀번호
@@ -26,15 +18,15 @@ var connection = mysql.createConnection({
 
 //mysql접속
 connection.connect();
-
-app.post('/user',function(req,res){
-  var attr_id=req.bodt.attr_id;
-  var 
-}
-  )
-
-connection.end();
+var sql='select * from geotable';
+connection.query(sql,function(err,rows){
+    if(err){
+        console.log(err);
+    }else {
+        console.log('rows',rows);
+    }
+})
 
 app.listen(3000,function(){
-  console.log('Express server listening on post ');
-});
+console.log('server is running...');
+})
