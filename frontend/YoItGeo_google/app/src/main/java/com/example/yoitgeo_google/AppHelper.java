b2702b23bfd8e2cf45c66c1d7a8d913aec76beff
dokdokorea <7dpdud@naver.com>
2021-01-11 18:54:56 +0900

    connect nodejs with android using volley

---
+++ frontend/YoItGeo_google/app/src/main/java/com/example/yoitgeo_google/AppHelper.java
@@ -0,0 +1,7 @@
+package com.example.yoitgeo_google;
+
+import com.android.volley.RequestQueue;
+
+public class AppHelper {
+    public static RequestQueue requestQueue;
+}
--- frontend/YoItGeo_google/app/src/main/java/com/example/yoitgeo_google/DisplayCommentActivity.java
+++ frontend/YoItGeo_google/app/src/main/java/com/example/yoitgeo_google/DisplayCommentActivity.java
@@ -3,21 +3,79 @@ package com.example.yoitgeo_google;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class DisplayCommentActivity extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class DisplayCommentActivity extends AppCompatActivity {
+    TextView geo_name;
+    TextView geo_exp;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_comment);
 
-        TextView geo_name = (TextView) findViewById(R.id.geo_name);
-        TextView geo_exp = (TextView) findViewById(R.id.geo_exp);
-        
+        geo_name = (TextView) findViewById(R.id.geo_name);
+        geo_exp = (TextView) findViewById(R.id.geo_exp);
+        button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                sendRequest();
+            }
+        });
+        if(AppHelper.requestQueue == null){
+            AppHelper.requestQueue= Volley.newRequestQueue(getApplicationContext());
+        }
 
 
+    }
+
+    public void sendRequest(){
+        String url = "http://192.168.219.100:3000/geosite";
+
+
+        StringRequest request = new StringRequest(Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        println(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        println("에러다 =>" + error.getMessage());
+                    }
+                }){
+            @Override
+            protected Map<String,String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String,String>();
+                return params;
+            }
+        };
+
+        request.setShouldCache(false);
+        AppHelper.requestQueue.add(request);
+    }
+
+    public void println(String data){
+        geo_exp.append(data+"\n");
     }
 }
