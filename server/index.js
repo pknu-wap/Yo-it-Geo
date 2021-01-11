54b3e68971ebd6a8d0d301cb8c58dd92d0c4f05d
dokdokorea <7dpdud@naver.com>
2021-01-11 18:42:55 +0900

    connect to server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'gloriay37!',
+    password: '0000',
     database: 'yoitgeo'
 });
 
